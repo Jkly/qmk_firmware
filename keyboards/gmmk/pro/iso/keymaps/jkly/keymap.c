87ac9b9c80d127caf16e815b86f0445d60a2cef3
jkly <jkly@users.noreply.github.com>
2022-02-05 15:37:39 +0000

    Move encoder into my keymap

--- keyboards/gmmk/pro/iso/keymaps/jkly/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/jkly/keymap.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return true;
+}
 
 /* THIS FILE WAS GENERATED!
  *
