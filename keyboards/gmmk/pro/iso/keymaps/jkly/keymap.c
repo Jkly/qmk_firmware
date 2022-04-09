d87d1734c5baca441ba0e5bed4e2488aa26a2a11
jkly <jkly@users.noreply.github.com>
2022-04-09 17:20:10 +0100

    Use ENCODER_ENABLE from base

--- keyboards/gmmk/pro/iso/keymaps/jkly/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/jkly/keymap.c
@@ -1,14 +1,5 @@
 #include QMK_KEYBOARD_H
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-    return true;
-}
-
 #ifdef VIA_ENABLE
     #define USER_START USER00
 #else
